149c621b1896a834549fbd73fd9151e5d71978d8
alvizojc1104 <mikealvizo44@gmail.com>
2025-02-05 11:12:04 +0800

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { Toaster } from "@/components/ui/sonner";
 import Home from "./pages/home";
 import Movie from "./pages/movie";
 
@@ -12,7 +11,6 @@ const App = () => {
 					<Route path="/" element={<Home />} />
 					<Route path="/movie/:movieId" element={<Movie />} />
 				</Routes>
-				<Toaster richColors theme="light" position="top-center" />
 			</HelmetProvider>
 		</Router>
 	);
